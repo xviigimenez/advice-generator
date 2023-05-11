b68aca25872db09e7c84f5ef9884a76fd1a1f836
xviigimenez <gimenez@tuta.io>
2023-05-11 20:57:12 -0300

    configuração tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+          blue: {
+              dark: "hsl(218, 23%, 16%)",
+              gray: "hsl(217, 19%, 38%)",
+              dark_gray: "hsl(217, 19%, 24%)",
+          },
+          light_cyan: "hsl(193, 38%, 86%)",
+          neon_green: "hsl(150, 100%, 66%)",
       },
     },
   },
