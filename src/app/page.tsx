7eccdff00e1ef41819db821dbe008318bdbe8131
xviigimenez <gimenez@tuta.io>
2023-05-12 12:58:19 -0300

    começada a estruturação da página

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-	return <main>cachorro</main>;
+	return (
+        <main>
+            <article>
+                <div>adive</div>
+                <div>it is eas yto set up</div>
+                <div>
+                    <img src="/pattern-divider-mobile.svg">
+                    <button><img src="/icon-dice.svg"></button>
+                </div>
+            </article>
+        </main>
+    );
 }
