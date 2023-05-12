d1d990c3df32e84cff615492eb07b58c8df5ccbe
xviigimenez <gimenez@tuta.io>
2023-05-12 14:45:40 -0300

    terminada a estilzação da página

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
 	return (
-        <main>
-            <article>
-                <div>adive</div>
-                <div>it is eas yto set up</div>
-                <div>
-                    <img src="/pattern-divider-mobile.svg">
-                    <button><img src="/icon-dice.svg"></button>
+        <main className="bg-blue-dark w-screen h-screen flex items-center justify-center">
+            <article className="bg-blue-dark_gray flex flex-col items-center rounded-xl gap-6 p-4">
+                <div className="mt-6"><h1 className="text-neon_green uppercase text-sm tracking-[.2em] font-bold">adive</h1></div>
+                <div><h2 className="text-light_cyan text-xl font-bold">it is eas yto set up</h2></div>
+                <div className="flex flex-col items-center">
+                    <img src="/pattern-divider-mobile.svg"/>
+                    <button className="p-4 bg-neon_green rounded-full translate-y-[85%]"><img src="/icon-dice.svg"/></button>
                 </div>
             </article>
         </main>
