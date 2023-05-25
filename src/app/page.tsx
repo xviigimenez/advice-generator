618f51cc81e8025252b1e706af0ff9a89cf9293c
Guilherme Figueira <guibf2004@gmail.com>
2023-05-25 16:35:22 -0300

    eu nao acredito 🤝chatgpt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,28 @@
-import Button from "./components/Button";
+"use client";
+import { useState, useEffect } from "react";
 
 async function getAdvice() {
-	"use server";
 	const advice = await fetch("https://api.adviceslip.com/advice", {
 		cache: "no-cache",
 	});
-
 	return advice.json();
 }
 
-export default async function Home() {
-	const advice = await getAdvice();
+export default function Home() {
+	const [advice, setAdvice] = useState({ slip: { id: "", advice: "" } });
 
+	const fetchData = async () => {
+		const newAdvice = await getAdvice();
+		setAdvice(newAdvice);
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, []); // Fetch data on component mount
+
+	const handleButtonClick = async () => {
+		await fetchData();
+	};
 	return (
 		<article className="bg-blue-dark_gray flex flex-col items-center rounded-xl gap-6 p-4">
 			<div className="mt-6">
@@ -26,7 +37,12 @@ export default async function Home() {
 			</div>
 			<div className="flex flex-col items-center">
 				<img src="/pattern-divider-mobile.svg" />
-				<Button getAdvice={getAdvice} />
+				<button
+					className="p-4 bg-neon_green rounded-full translate-y-[85%] hover:shadow-[0_0_25px_0] hover:shadow-neon_green transition-all"
+					onClick={handleButtonClick}
+				>
+					<img src="/icon-dice.svg" />
+				</button>
 			</div>
 		</article>
 	);
