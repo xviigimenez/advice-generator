0215bb4ed4cf2992f0fe92cf686b82929bfd8586
Guilherme Figueira <guibf2004@gmail.com>
2023-07-25 13:51:27 -0300

    quase acabado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,63 @@
 "use client";
-import { useState, useEffect } from "react";
+import axios from "axios";
+import Image from "next/image";
+import { useQuery } from "react-query";
 
-async function getAdvice() {
-	const advice = await fetch("https://api.adviceslip.com/advice", {
-		cache: "no-cache",
-	});
-	return advice.json();
+interface AdviceData {
+	slip: { id: number; advice: string };
 }
 
 export default function Home() {
-	const [advice, setAdvice] = useState({ slip: { id: "", advice: "" } });
+	const { data, isLoading, isSuccess, refetch } = useQuery({
+		queryFn: async () => {
+			const { data } = await axios.get(
+				"https://api.adviceslip.com/advice"
+			);
+			return data as AdviceData;
+		},
+	});
 
-	async function fetchData() {
-		const newAdvice = await getAdvice();
-		setAdvice(newAdvice);
-	}
-
-	useEffect(() => {
-		fetchData();
-	}, []);
-
-	async function handleButtonClick() {
-		await fetchData();
-	}
 	return (
-		<article className="bg-blue-dark_gray flex flex-col items-center rounded-xl gap-6 p-4">
+		<article className="bg-blue-dark_gray flex flex-col items-center rounded-xl gap-6 p-4 xl:min-w-max">
 			<div className="mt-6">
 				<h1 className="text-neon_green uppercase text-sm tracking-[.2em] font-bold">
-					advice # {advice.slip.id}
+					{isLoading ? (
+						"Loading..."
+					) : isSuccess ? (
+						<span>advice# {data.slip.id}</span>
+					) : (
+						"Error!"
+					)}
 				</h1>
 			</div>
 			<div>
 				<h2 className="text-light_cyan text-xl font-bold">
-					{advice.slip.advice}
+					{isLoading ? (
+						"Loading..."
+					) : isSuccess ? (
+						<span>{data.slip.advice}</span>
+					) : (
+						"Error!"
+					)}
 				</h2>
 			</div>
 			<div className="flex flex-col items-center">
-				<img src="/pattern-divider-mobile.svg" />
+				<Image
+					src="/pattern-divider-mobile.svg"
+					alt={"Pattern divider"}
+					width={295}
+					height={16}
+				/>
 				<button
 					className="p-4 bg-neon_green rounded-full translate-y-[85%] hover:shadow-[0_0_25px_0] hover:shadow-neon_green transition-all"
-					onClick={handleButtonClick}
+					onClick={() => refetch()}
 				>
-					<img src="/icon-dice.svg" />
+					<Image
+						src="/icon-dice.svg"
+						alt={""}
+						width={24}
+						height={24}
+					/>
 				</button>
 			</div>
 		</article>
