bf2e0b626440f91fad666368f4ea0590a1ed9492
Guilherme Figueira <guibf2004@gmail.com>
2023-07-31 11:26:52 -0300

    agr sim

---
+++ src/app/AdviceBox.tsx
@@ -0,0 +1,73 @@
+"use client";
+import axios from "axios";
+import Image from "next/image";
+import { useQuery } from "react-query";
+
+interface AdviceData {
+	slip: { id: number; advice: string };
+}
+
+export default function AdviceBox() {
+	const { data, isLoading, isSuccess, refetch } = useQuery({
+		queryFn: async () => {
+			const { data } = await axios.get(
+				"https://api.adviceslip.com/advice"
+			);
+			return data as AdviceData;
+		},
+		refetchOnWindowFocus: false,
+	});
+	return (
+		<>
+			<div className="mt-6">
+				<h1 className="text-neon_green uppercase text-sm tracking-[.2em] font-bold">
+					{isLoading ? (
+						"Loading..."
+					) : isSuccess ? (
+						<span>advice# {data.slip.id}</span>
+					) : (
+						"Error!"
+					)}
+				</h1>
+			</div>
+			<div>
+				<h2 className="text-light_cyan text-xl font-bold">
+					{isLoading ? (
+						"Loading..."
+					) : isSuccess ? (
+						<span>{data.slip.advice}</span>
+					) : (
+						"Error!"
+					)}
+				</h2>
+			</div>
+			<div className="flex flex-col items-center">
+				<Image
+					src="/pattern-divider-mobile.svg"
+					alt={"Pattern divider"}
+					className="xl:hidden"
+					width={295}
+					height={16}
+				/>
+				<Image
+					src="/pattern-divider-desktop.svg"
+					alt={"Pattern divider"}
+					className="hidden xl:block"
+					width={295}
+					height={16}
+				/>
+				<button
+					className="p-4 bg-neon_green rounded-full translate-y-[85%] hover:shadow-[0_0_25px_0] hover:shadow-neon_green transition-all"
+					onClick={() => refetch()}
+				>
+					<Image
+						src="/icon-dice.svg"
+						alt={"Dice icon"}
+						width={24}
+						height={24}
+					/>
+				</button>
+			</div>
+		</>
+	);
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-"use client";
-import { QueryClient, QueryClientProvider } from "react-query";
 import "./globals.css";
 import { Manrope } from "next/font/google";
 
@@ -8,7 +6,7 @@ const manrope = Manrope({ subsets: ["latin"] });
 export const metadata = {
 	title: "Advice Generator | Frontend Mentor",
 };
-const queryClient = new QueryClient();
+
 export default function RootLayout({
 	children,
 }: {
@@ -19,9 +17,7 @@ export default function RootLayout({
 			<body
 				className={`${manrope.className} bg-blue-dark w-screen h-screen flex items-center justify-center`}
 			>
-				<QueryClientProvider client={queryClient}>
-					{children}
-				</QueryClientProvider>
+				{children}
 			</body>
 		</html>
 	);
